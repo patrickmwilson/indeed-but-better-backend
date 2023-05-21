74ed00f967bb9b1be67e794c9fa590aaf535970e
patrickmwilson <patrickmwilson@ucla.edu>
2023-05-21 13:38:32 -0700

    base project structure

---
+++ src/main/java/com/personal/indeedbutbetterbackend/IndeedButBetterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personal.indeedbutbetterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndeedButBetterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndeedButBetterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/indeedbutbetterbackend/IndeedButBetterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.indeedbutbetterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndeedButBetterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
